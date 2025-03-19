fa4adfa4a310caf20895d0bd88442514c06244d0
sosPro300 <t.virassamy270@gmail.com>
2025-03-19 16:34:25 +0100

    Add files via upload

---
+++ SOSPro2/Conversation.swift
@@ -0,0 +1,104 @@
+//
+//  Conversation.swift
+//  SOSPro2
+//
+//  Created by Apprenant 130 on 19/03/2025.
+//
+import SwiftUI
+
+struct LaConversation: View {
+    @State private var message: String = ""
+    var userPro: User
+    @State var isModal = false
+    var body: some View {
+        VStack {
+            HStack(spacing:50) {
+                //a changer
+                Image("\(userPro.image)")
+                    .resizable()
+                    .scaledToFit()
+                    .clipShape(Circle())
+                    .frame(width: 110)
+//
+                VStack (spacing: 5){ //marie,cert et plombière, butt
+                    HStack (spacing:8){ // marie et cert
+                        Text(userPro.prenom + " " + userPro.nom)
+                            .bold()
+                            .font(.title2)
+                        Circle().frame(width: 9).foregroundStyle(userPro.enLigne ? .green : .red)
+                        //if userPro.typeUser.estCer
+                        
+                    }//Fin hstack marie, cert
+                    
+                    // changement
+                    HStack {
+                        Text (userPro.typeUser.domaine)
+                        if userPro.typeUser.estCertif {
+                            Image(systemName: "checkmark.seal.fill").resizable().frame(width: 15, height: 15)
+                        }
+                    }
+                }
+            }
+            HStack  { // des deux button
+                Button {
+                    isModal = true
+                } label: {Text("Laisser un avis")
+                        .bold()
+                        .padding(10)
+                        .background(Color.accent)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .font(.footnote)
+                        .offset(x:90)
+                }//fin button1
+             
+                //button archive:
+               /* Button {} label: {Text("Archiver")
+                        .bold()
+                        .padding()
+                        .background(Color.accent)
+                        .foregroundColor(.white)
+                        .cornerRadius(10)
+                        .font(.footnote)
+                        .offset(x:50)
+                }//fin button archive*/
+            }
+            //.padding(.top, 40)
+            //.padding(.horizontal)
+
+            Divider()
+                .background(Color.gray)
+                .padding(.top)
+
+            
+            Spacer()
+            HStack (spacing: 0){   /*envoi message*/
+                Button {
+                    
+                } label: {
+                    Image(systemName:"plus.circle")
+                        .resizable()
+                        .frame(width: 30, height: 30)
+                        .foregroundStyle(.accent)
+                        .bold()
+                }//fin Button 
+                TextField("Écrire un message...", text: $message)
+                    .textFieldStyle(RoundedBorderTextFieldStyle()) // Ajoute un style
+                    .padding()
+                Image(systemName: "paperplane.circle.fill")
+                    .resizable()
+                    .frame(width: 28, height: 28)
+                    .foregroundStyle(.accent)
+                    .bold()
+            }
+            .padding()
+        
+    }.sheet(isPresented: $isModal) {
+        LaisserAvis(pro:userPro)
+    }//fin Vstack principale
+    }//fin body
+}//fin structure
+
+#Preview {
+    LaConversation(userPro: proA)
+}
---
+++ SOSPro2/Messagerie.swift
@@ -0,0 +1,89 @@
+//
+//  Conversation.swift
+//  SOSPro2
+//
+//  Created by Apprenant 130 on 19/03/2025.
+//
+import SwiftUI
+
+struct Messagerie: View {
+    //@State var maConversation : [Conversation] = [conversation1]
+   // @State var listMessages: [Message] = [message1, message2, message3]
+    @State var profil: User
+    var body: some View {
+        VStack{
+            NavigationStack {
+                Text("Mes messages")
+                    .font(.largeTitle)
+                    .padding()
+                Spacer()
+                List (listePros) { pro in
+                    NavigationLink(destination: LaConversation(userPro: pro)) { //profil.conversations.user
+                        VStack(alignment: .leading) {
+                            HStack (alignment: .top) {
+                                Image("\(pro.image)")
+                                    .resizable().scaledToFill() //Remplit bien le cercle sans déformation
+                                    .frame(width: 70, height: 70) //Définit une petite taille
+                                    .clipShape(Circle())
+                                VStack(alignment:.leading) {
+                                    HStack {
+                                        Text("\(pro.prenom) \(pro.nom)")
+                                            .font (.title2)
+                                            .bold()
+                                        Circle().fill(pro.enLigne ? .green : .red).frame(width:8, height: 8)
+                                    }
+                                    Text ("\(Date.now.formatted(.dateTime.day().month().hour().minute()))").font(.caption)
+                                        .padding(.trailing, 0)
+                                }.offset(y:10)
+                            }// hstack1
+                            Text("Aperçu du dernier message reçu")//.padding()
+                        }
+                    }  //Fin navlink
+                }      //fin list
+            }// fin Vstack
+        }//fin nav Stack
+    }//Fin body
+}//fin Structure
+
+/*#Preview {
+    Messagerie(profil:loggedUser)
+}*/
+
+class Conversation: ObservableObject, Identifiable{
+    @Published var id: UUID?
+    @Published var user: User
+    @Published var messages: [Message]
+    
+    init(id: UUID? = nil, user: User, messages: [Message]) {
+        self.id = id
+        self.user = user
+        self.messages = []
+    }
+}
+var conv1 = Conversation(user: proA, messages: [message2,message1])
+var conv2 = Conversation(user: proB, messages: [message4,message3])
+var conv3 = Conversation(user: proC, messages: [])
+var conv4 = Conversation(user: proA, messages: [])
+var conv5 = Conversation(user: proA, messages: [])
+class Message: ObservableObject, Identifiable{
+    @Published var id: UUID?
+    @Published var contenu: String
+    @Published var dateEnvoi: Date
+    @Published var heure: String
+    
+    init(id: UUID? = nil, contenu: String, dateEnvoi: Date, heure: String) {
+        self.id = id
+        self.contenu = contenu
+        self.dateEnvoi = dateEnvoi
+        self.heure = heure
+    }
+}
+
+
+// Instances Message
+var message1 = Message(contenu: "Bonjour...", dateEnvoi: Date(), heure: "10:00")
+var message2 = Message(contenu: "Comment ça va ?", dateEnvoi: Date(), heure: "10:01")
+var message3 = Message(contenu: "Hello!", dateEnvoi: Date(), heure: "11:00")
+var message4 = Message(contenu: "Comment ça va ?", dateEnvoi: Date(), heure: "11:03")
+var message5 = Message(contenu: "Bonjour!", dateEnvoi: Date(), heure: "12:00")
+var message6 = Message(contenu: "Comment ça va ?", dateEnvoi: Date(), heure: "12:01")
